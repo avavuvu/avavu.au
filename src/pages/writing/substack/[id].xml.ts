79a5ce265a884fc91d2b4b1b6fd1cf16e46c65b1
Ava Vu <avadinhvu@gmail.com>
2025-10-06 22:40:49 +1100

    added hero to substack

--- src/pages/writing/substack/[id].xml.ts
+++ src/pages/writing/substack/[id].xml.ts
@@ -1,4 +1,4 @@
-import rss from '@astrojs/rss';
+import rss, { type RSSFeedItem } from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection, getEntry } from 'astro:content';
 import Markdoc from "@markdoc/markdoc"
@@ -15,8 +15,19 @@ export const GET: APIRoute = async ({site, params}) => {
     const { id } = params
     const post = await getEntry("writing", id!)!;
 
+    let hero = ""
+    if(post.data.heroImageSrc) {
+        if(typeof post.data.heroImageSrc === "string") {
+            hero = `![${post.data.heroImageAlt}](${post.data.heroImageSrc})`
+        } else {
+            hero = `![${post.data.heroImageAlt}](${post.data.heroImageSrc[0]})`
+        }
+    }
+
     const note = `*This is a mirror. This article was first published and is better read at [avavu.au](https://avavu.au/writing/${post.id})*`
-    const article = `${note}
+    const article = `${hero}
+    
+    ${note}
 
     ${post.body!}`
     
@@ -53,17 +64,19 @@ export const GET: APIRoute = async ({site, params}) => {
     
     const htmlString = Markdoc.renderers.html(tree)
 
-    const item = {
+    const item: RSSFeedItem = {
         title: post.data.title,
         pubDate: post.data.date,
         description: post.data.description,
         link: `/writing/${post.id}/`,
-        content: htmlString
+        content: htmlString,
+        categories: post.data.tags,
+        
     }
     
     return rss({
         title: "Substack import",
-        description: "This is an automated endpoint for the purpose of making importing onto substack easier. If you are reading this you must be very clever. Or you are literally me.",
+        description: "This is an automated endpoint for the purpose of making importing on to substack easier. If you are reading this you must be very clever. Or you are literally me.",
         site: site!,
         items: [item]
     })
