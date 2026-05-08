75c7e1336ab2867d06f2b053cbc0bac519aa12b1
Ava Vu <avadinhvu@gmail.com>
2026-05-08 19:01:39 +1000

    fixed newsletter image dir

--- scripts/convertNewsletters/index.ts
+++ scripts/convertNewsletters/index.ts
@@ -32,25 +32,25 @@ const convertNewsletters = async () => {
 		await writeFile(outPath, updated)
 	}
 
-	// await Promise.all(
-	// 	images.map(async ({ original, webp }) => {
-	// 		const absolutePath = `${BASE_DIR}/old/${original}`
+	await Promise.all(
+		images.map(async ({ original, webp }) => {
+			const absolutePath = `${BASE_DIR}/old/${original}`
 
-	// 		try {
-	// 			const file = await readFile(absolutePath)
-	// 			const fileName = original.split('/').at(-1)!.split('.')[0]
-	// 			const outPath = `scripts/convertNewsletters/new/images/newsletter/${fileName}.webp`
+			try {
+				const file = await readFile(absolutePath)
+				const fileName = original.split('.')[0]
+				const outPath = `scripts/convertNewsletters/new/${fileName}.webp`
 
-	// 			console.log('writing to:', outPath)
-	// 			await mkdir(dirname(outPath), { recursive: true })
-	// 			await sharp(file)
-	// 				.webp({ nearLossless: true, quality: 67, effort: 6 })
-	// 				.toFile(outPath)
-	// 		} catch (e) {
-	// 			console.error('failed on:', absolutePath, e)
-	// 		}
-	// 	})
-	// )
+				console.log('writing to:', outPath)
+				await mkdir(dirname(outPath), { recursive: true })
+				await sharp(file)
+					.webp({ nearLossless: true, quality: 67, effort: 6 })
+					.toFile(outPath)
+			} catch (e) {
+				console.error('failed on:', absolutePath, e)
+			}
+		})
+	)
 }
 
 convertNewsletters()
